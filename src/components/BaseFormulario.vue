9e600c5e54f77d62e8b4e250ac3b2a9626caca96
renancreuso <renan_almeida@ufms.br>
2023-09-12 22:53:43 -0300

    criando componente Temporizador

--- src/components/BaseFormulario.vue
+++ src/components/BaseFormulario.vue
@@ -13,26 +13,7 @@
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <!-- passando valor para o filho, componente possui propriedade chamada tempoEmSegundo que atribuimos tempoEmSegundo -->
-          <BaseCronometro :tempoEmSegundo="tempoEmSegundo" /> 
-
-          <button @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-
-          <button @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <BaseTemporizador/>
       </div>
     </div>
   </div>
@@ -40,34 +21,16 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import BaseCronometro from "../components/BaseCronometro.vue";
+import BaseTemporizador from "./BaseTemporizador.vue";
+
 
 export default defineComponent({
   name: "BaseFormulario",
   components: {
-    BaseCronometro,
-  },
-  data() {
-    return {
-      tempoEmSegundo: 0,
-      cronometro: 0,
-      cronometroRodando: false,
-    };
-  },
+    BaseTemporizador
+},
+  
 
-  methods: {
-    iniciar() {
-      this.cronometroRodando = true;
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundo += 1;
-      }, 1000);
-      console.log("Iniciando");
-    },
-    finalizar() {
-      this.cronometroRodando = false;
-      clearInterval(this.cronometro);
-      console.log("Finalizando");
-    },
-  },
+ 
 });
 </script>
---
+++ src/components/BaseTemporizador.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <!-- passando valor para o filho, componente possui propriedade chamada tempoEmSegundo que atribuimos tempoEmSegundo -->
+    <BaseCronometro :tempoEmSegundo="tempoEmSegundo" />
+
+    <button @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+
+    <button @click="finalizar" :disabled="!cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseCronometro from "../components/BaseCronometro.vue";
+
+export default defineComponent({
+  name: "BaseTemporizador",
+  components: {
+    BaseCronometro,
+  },
+
+  data() {
+    return {
+      tempoEmSegundo: 0,
+      cronometro: 0,
+      cronometroRodando: false,
+    };
+  },
+
+    methods: {
+      iniciar() {
+        this.cronometroRodando = true;
+        this.cronometro = setInterval(() => {
+          this.tempoEmSegundo += 1;
+        }, 1000);
+        console.log("Iniciando");
+      },
+      finalizar() {
+        this.cronometroRodando = false;
+        clearInterval(this.cronometro);
+        console.log("Finalizando");
+      },
+    },
+  
+});
+</script>
