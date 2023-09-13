05c9f4c5619b9fd02916c263806de84c0a5b5994
renancreuso <renan_almeida@ufms.br>
2023-09-12 22:05:10 -0300

    deixando o cronometro funcional

---
+++ src/components/BaseCronometro.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+      <strong class="display">
+        {{ tempoDecorrido }}
+      </strong>
+    </section>
+  </template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+    name:'BaseCronometro',
+
+    data(){
+        return{
+
+        }
+    },
+    props:{
+        tempoEmSegundo:{
+            type: Number,
+            default: 0     
+        }
+    },
+    computed: {
+    tempoDecorrido(): string {
+      const horas = Math.floor(this.tempoEmSegundo / 3600);
+      const minutos = Math.floor((this.tempoEmSegundo % 3600) / 60);
+      const segundos = this.tempoEmSegundo % 60;
+
+      const horasFormatadas = horas.toString().padStart(2, "0");
+      const minutosFormatados = minutos.toString().padStart(2, "0");
+      const segundosFormatados = segundos.toString().padStart(2, "0");
+
+      return `${horasFormatadas}:${minutosFormatados}:${segundosFormatados}`;
+    },
+  },
+
+})
+    
+
+</script>
